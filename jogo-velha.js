947e0425150bfc0801fb91b686a2343203648a04
Paulo Ferreira <phgferreira@outlook.com>
2022-07-13 19:35:54 -0300

    Alterna entre jogadores

--- jogo-velha.js
+++ jogo-velha.js
@@ -1,10 +1,19 @@
 const X = "X";
 const O = "O";
 
+let rodada = 0
+
 function reiniciarJogo() {
     
 }
 
 function selecionarArea(posicaoLinha, posicaoColuna) {
+    
+    // Verifica quem está jogando
+    if (rodada%2 == 0) {
+        marcarJogadorAtivo(O)
+    } else {
+        marcarJogadorAtivo(X)
+    }
 
 }
