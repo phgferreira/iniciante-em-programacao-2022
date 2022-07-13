f835180a6f7152780b3e89ebdf44a911158d36ff
Paulo Ferreira <phgferreira@outlook.com>
2022-07-13 20:24:34 -0300

    Feito o calculo de vitória

--- jogo-velha.js
+++ jogo-velha.js
@@ -1,19 +1,53 @@
 const X = "X";
 const O = "O";
 
-let rodada = 0
+const matrix =  [[2,9,4],
+                 [7,5,3],
+                 [6,1,8]];
+
+var rodada = 0
+var pontosX = 0
+var pontosO = 0
 
 function reiniciarJogo() {
     
 }
 
 function selecionarArea(posicaoLinha, posicaoColuna) {
+    console.log('Rodada: ' + rodada)
     
     // Verifica quem está jogando
     if (rodada%2 == 0) {
+        desenharSimbolo(X, posicaoLinha, posicaoColuna)
+        pontosX = calculaJogada(X, pontosX, posicaoLinha-1, posicaoColuna-1)
+
+        // Próximo jogador
         marcarJogadorAtivo(O)
     } else {
+        desenharSimbolo(O, posicaoLinha, posicaoColuna)
+        pontosO = calculaJogada(O, pontosO, posicaoLinha-1, posicaoColuna-1)
+
+        // Próximo jogador
         marcarJogadorAtivo(X)
     }
 
+    rodada++
+}
+
+// Essa função calcula o peso de cada casa
+function calculaJogada(simbolo, pontos, linha, coluna) {
+    pontos += matrix[linha][coluna]
+
+    /* Se ambos os jogadores não tiver jogado pelo menos 2 vezes cada então não pode ter vitória
+       Se a soma do peso das casas for = 15 então ganhou */
+    if (rodada > 3 && pontos == 15){
+        exibirResultado('Vitória do ' + simbolo)
+    }
+    /* Se a situação atual não se enquadra em cima e os 15 pontos foram atingidos ou ultrapassado então
+       esse foi uma jogada inválida e deve voltar atras apenas nos calculos e não no tabuleiro */
+    else if (pontos >= 15) {
+        pontos -= matrix[linha][coluna]
+    }
+
+    return pontos
 }
